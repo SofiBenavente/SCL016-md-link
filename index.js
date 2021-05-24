058a9afde5c2c8f3dbf628c3a6ed758b7dccc417
SofiBenavente <sofia.benavente@gmail.com>
2021-05-24 10:35:47 -0400

    node fetch y markdown link extractor

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 module.exports = () => {
   // ...
 };
+/*
+* Leer todo el contenido de un archivo
+* con Node.JS y fs
+*
+* @author parzibyte
+*/
+const fs = require("fs"),
+NOMBRE_ARCHIVO = "index.js";
+
+fs.readFile(NOMBRE_ARCHIVO, 'utf8', (error, datos) => {
+if (error) throw error;
+console.log("El contenido es: ", datos);
+}); 
